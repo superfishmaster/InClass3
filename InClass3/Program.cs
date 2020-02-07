a7d5a73789a4e157a4405dabf238ac0cabcfe0ad
雪 风 <a1227067902@outlook.com>
2020-02-07 11:49:06 -0500

    Add project files.

---
+++ InClass3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace InClass3
+{
+    // Create a Car class
+    class Car
+    {
+        public string model;  // Create a field
+
+        // Create a class constructor for the Car class
+        public Car()
+        {
+            model = "Mustang"; // Set the initial value for model
+        }
+
+        static void Main(string[] args)
+        {
+            Car Ford = new Car();  // Create an object of the Car Class (this will call the constructor)
+            Console.WriteLine(Ford.model);  // Print the value of model
+        }
+    }
+
+    // Outputs "Mustang"
+
+}
+
+
+
